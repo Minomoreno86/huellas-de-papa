571040fee95995c7baee6edac7cb7a56cfbac1b7
Jorge Vasquez <minomoreno86@gmail.com>
2025-10-03 17:52:33 -0500

    Initial Commit

---
+++ Huellas de PapaTests/Huellas_de_PapaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Huellas_de_PapaTests.swift
+//  Huellas de PapaTests
+//
+//  Created by Jorge Vasquez rodriguez on 3/10/25.
+//
+
+import Testing
+@testable import Huellas_de_Papa
+
+struct Huellas_de_PapaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
